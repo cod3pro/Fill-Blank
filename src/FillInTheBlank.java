5175db4006aede8992d7e7ff7815e971c3b4a496
Borhan <net2net4all@gmail.com>
2017-05-06 14:13:59 -0700

    Made the initial code

---
+++ src/FillInTheBlank.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+
+
+public class FillInTheBlank {
+
+	public static void main(String[] args){
+		new JOptionPane();
+		
+	}
+	
+	
+	public FillInTheBlank(){
+		
+	}
+}
